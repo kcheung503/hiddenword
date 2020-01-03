0fc6aaf6368f5c4e8dd067da1d9f14f7ef5bd121
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2020-01-03 09:23:17 -0500

    final commit

--- hiddenword.java
+++ hiddenword.java
@@ -1,4 +1,63 @@
-public class apcsarighttriangle {
+public class hiddenword {
     public static void main(String[] args) {
-      
+
+    hiddenword() {
+      HiddenWord puzzle = new HiddenWord("KYLEC");
+
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("Guess the word: ");
+      String guess = in.nextString();
+
+      in.close()
+
+      getHint() {
+        if (K.substring(0,1)equalsSubstring(0,1)) {
+          //mark the letter with its original value
+        } else if (K.substring(0,1)equalssubstring(1,2) || substring(2,3) || substring(3,4) || substring(4,5) {
+          //mark the original guessed space with a +
+        } else {
+          //mark the letter with a *
+        }
+
+        if (Y.substring(0,1)equalsSubstring(0,1)) {
+          //mark the letter with its original value
+        } else if (Y.substring(0,1)equalssubstring(1,2) || substring(2,3) || substring(3,4) || substring(4,5) {
+          //mark the original guessed space with a +
+        } else {
+          //mark the letter with a *
+        }
+
+        if (L.substring(0,1)equalsSubstring(0,1)) {
+          //mark the letter with its original value
+        } else if (L.substring(0,1)equalssubstring(1,2) || substring(2,3) || substring(3,4) || substring(4,5) {
+          //mark the original guessed space with a +
+        } else {
+          //mark the letter with a *
+        }
+
+        if (E.substring(0,1)equalsSubstring(0,1)) {
+          //mark the letter with its original value
+        } else if (E.substring(0,1)equalssubstring(1,2) || substring(2,3) || substring(3,4) || substring(4,5) {
+          //mark the original guessed space with a +
+        } else {
+          //mark the letter with a *
+        }
+
+        if (C.substring(0,1)equalsSubstring(0,1)) {
+          //mark the letter with its original value
+        } else if (C.substring(0,1)equalssubstring(1,2) || substring(2,3) || substring(3,4) || substring(4,5) {
+          //mark the original guessed space with a +
+        } else {
+          //mark the letter with a *
+        }
+
+        if (kylec.substring(0, 5)equalspuzzle.subsctring(0, 5) {
+          System.out.println("You win!");
+        } else {
+          hiddenword();
+        }
+      }
+    }
+  }
 }
